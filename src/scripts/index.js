c69985457f918b6269d7b11a9682428c277a2fb7
Abuzar <abuzar28102004@gmail.com>
2023-03-14 19:18:18 +0300

    feat: create hero page (intro)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,17 @@
-const burgerOverlay = document.querySelector(".burger-overlay");
+const burgerOverlay = document.querySelector('.burger-overlay');
 
-const burgerBtn = document.querySelector(".header__burger-btn");
-const exitText = burgerBtn.querySelector(".header__burger-btn-text");
+const burgerBtn = document.querySelector('.header__burger-btn');
+const exitText = burgerBtn.querySelector('.header__burger-btn-text');
 
 const togglePopup = () => {
-  burgerOverlay.classList.toggle("hidden");
-  burgerBtn.classList.toggle("header__burger-btn_type_exit");
-  exitText.classList.toggle("hidden");
+  burgerOverlay.classList.toggle('hidden');
+  burgerBtn.classList.toggle('header__burger-btn_type_exit');
+  exitText.classList.toggle('hidden');
 };
 
 const toggleBurger = () => {
-  burgerBtn.classList.add("header__burger-btn_type_exit");
+  burgerBtn.classList.add('header__burger-btn_type_exit');
 };
 
-burgerBtn.addEventListener("click", togglePopup);
-exitText.addEventListener("click", toggleBurger);
+burgerBtn.addEventListener('click', togglePopup);
+exitText.addEventListener('click', toggleBurger);
